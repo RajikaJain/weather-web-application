eaabbb3ca06f417f0c31688c228fc3127cd3c05e
Rajika <rajikajain2@gmail.com>
2021-04-27 14:22:17 +0530

    project complete

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 #edit
 import urllib.request
 import json
+import time
 
 def index(request):
     if request.method == 'POST':
@@ -11,7 +12,7 @@ def index(request):
         source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' + city +'&units=metric&appid=976baac1ecd42c2b27a7f32340d285d8').read()
         list_of_data = json.loads(source)
         data ={
-            "country_code": str(list_of_data['sys'][çountry]),
+            "country_code": str(list_of_data['sys']['country']),
             "coordinate": str(list_of_data['coord']['lon']) + ',' + str(list_of_data['coord']['lat']),
             "main":  str(list_of_data['weather'][0]['main']),
             "temp": str(list_of_data['main']['temp']),
@@ -20,13 +21,13 @@ def index(request):
             "pressure": str(list_of_data['main']['pressure']),
 
             "humidity": str(list_of_data['main']['humidity']),
-
+            "clouds": str(list_of_data['clouds']['all']),
             "wind": str(list_of_data['wind']['speed']),
-            "sunrise": str(list_of_data['sys']['sunrise']),
-            "sunset": str(list_of_data['sys']['sunset']),
-
+            "sunrise": str(time.strftime("%I:%M:%S",time.gmtime(list_of_data['sys']['sunrise']-21600))),
+            "sunset":  str(time.strftime("%I:%M:%S",time.gmtime(list_of_data['sys']['sunset']-21600))),
+              "name": str(list_of_data['name']),
             
-            "description": str(list_of_data['weather'][0]['description'])
+            "description": str(list_of_data['weather'][0]['description']),
             "icon": list_of_data['weather'][0]['icon'],
 
         }
--- weatherProject/settings.py
+++ weatherProject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'weatherApp'
+    'weatherApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+import os
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 STATICFILES_DIRS =(
